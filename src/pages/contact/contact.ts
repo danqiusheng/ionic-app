886815f781ba038645ede03c4ea5034a6d544075
fp <342834999@qq.com>
2018-07-13 14:13:19 +0800

    contact页面

---
+++ src/pages/contact/contact.ts
@@ -0,0 +1,27 @@
+import {Component} from '@angular/core';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
+import {AboutPage} from "../about/about";
+
+/**
+ * Generated class for the ContactPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-contact',
+  templateUrl: 'contact.html',
+
+})
+export class ContactPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ContactPage');
+  }
+
+}
